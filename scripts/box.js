ed72e3716b24a02890ed9302261b6079d0785bd3
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-04-25 21:57:04 -0400

    GT5 part selection style; Removed on/off

--- scripts/box.js
+++ scripts/box.js
@@ -17,17 +17,5 @@ function playClick(){
 }
 
 function openBox(boxClicked){
-    const box = document.getElementById(boxClicked)
-    if(box.className === "box"){
-        box.className = "box open";
-        const tab = document.createElement("p");
-        tab.className = "tab";
-        tab.id = boxClicked + "tab"
-        box.appendChild(tab);
-    }
-    else if(box.className === "box open"){
-        box.className = "box";
-        const tabId = document.getElementById(boxClicked + "tab");
-        box.removeChild(tabId);
-    }
+    
 }
