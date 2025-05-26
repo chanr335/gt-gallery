54085d15e548b4bdf00f8db8ed260f4801012c4f
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-05-25 23:20:59 -0400

    toggle options

--- scripts/options.js
+++ scripts/options.js
@@ -1,54 +1,43 @@
-const options = document.getElementById("options");
+const optionSection = document.getElementById("optionSection");
 
 const scrollbarOption = document.getElementById("scrollbarOption");
 const scrollbar = document.getElementById("scrollbarSection");
+const optionLight1 = document.getElementById("optionLight1");
 
 const radioOption = document.getElementById("radioOption");
 const radio = document.getElementById("radioSection")
+const optionLight2 = document.getElementById("optionLight2");
 
 const infoOption = document.getElementById("infoOption");
 const info = document.getElementById("infoSection")
+const optionLight3 = document.getElementById("optionLight3");
 
-let optionsOn = false;
-let scrollbarOn = false;
-let radioOn = false;
-let infoOn = false;
 
-options.addEventListener("click", () => {
-    optionsOn = !optionsOn;
-    if(optionsOn){
-        options.classList.add("open");
-        scrollbarOption.classList.remove("hidden")
-        radioOption.classList.remove("hidden")
-        infoOption.classList.remove("hidden")
-    } else{
-        options.classList.remove("open");
-        scrollbarOption.classList.add("hidden")
-        radioOption.classList.add("hidden")
-        infoOption.classList.add("hidden")
+optionSection.addEventListener("click", (e) => {
+    if(e.target === optionSection){ //click exactly on options
+        optionSection.classList.toggle("open");
+        scrollbarOption.classList.toggle("hidden")
+        optionLight1.classList.toggle("hidden");
+
+        radioOption.classList.toggle("hidden")
+        optionLight2.classList.toggle("hidden");
+
+        infoOption.classList.toggle("hidden")
+        optionLight3.classList.toggle("hidden");
     }
 });
 
 scrollbarOption.addEventListener("click", () => {
-    scrollbarOn = !scrollbarOn
-    toggleOn(scrollbar, scrollbarOn);
+    scrollbar.classList.toggle("hidden");
+    optionLight1.classList.toggle("on");
 });
 
 radioOption.addEventListener("click", () => {
-    radioOn = !radioOn
-    toggleOn(radio, radioOn);
+    radio.classList.toggle("hidden");
+    optionLight2.classList.toggle("on");
 });
 
 infoOption.addEventListener("click", () => {
-    infoOn = !infoOn
-    toggleOn(info, infoOn);
+    info.classList.toggle("hidden");
+    optionLight3.classList.toggle("on");
 });
-
-function toggleOn(object, objectBool){
-    if(objectBool){
-        object.classList.remove("hidden")
-    }
-    else{
-        object.classList.add("hidden")
-    }
-}
